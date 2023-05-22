711daf5365f63c5e4dcea871a9a7a7f4cd49b716
Nikita Mayorov <nsmayorov@gmail.com>
2023-05-22 13:45:31 +0300

    add gitignore, first commit

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,14 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
 
 function App() {
   return (
     <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
+      <Logo/>
     </div>
   );
 }
 
 export default App;
+
