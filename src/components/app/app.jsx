16fd0ab06aabb1a2ee8921ec303b84b5e0096167
Nikita Mayorov <nsmayorov@gmail.com>
2023-05-24 22:33:34 +0300

    add BurgerIngredients

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,16 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
 import AppHeader from "./AppHeader/AppHeader";
+import BurgerIngredients from "./BurgerIngredients/BurgerIngredients"
 
 function App() {
   return (
     <div className={styles.app}>
       <AppHeader/>
+      <main className={styles.main}>
+        <BurgerIngredients/>
+        
+      </main>
     </div>
   );
 }
