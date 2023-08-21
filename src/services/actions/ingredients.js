b4ca8d1fa3243d09241e0300a803eb38dabdb865
imynel <nsmayorov@gmail.com>
2023-08-21 13:47:58 +0300

    change PC

--- src/services/actions/ingredients.js
+++ src/services/actions/ingredients.js
@@ -4,7 +4,7 @@ export const GET_INGREDIENTS_REQUEST = 'GET_INGREDIENTS_REQUEST'
 export const GET_INGREDIENTS_SUCCESS = 'GET_INGREDIENTS_SUCCESS'
 export const GET_INGREDIENTS_FAILED = 'GET_INGREDIENTS_FAILED'
 
-
+debugger
 export const getAllIngredients = () => {
     debugger
     return function(dispatch) {
