c7436289af7ca58ac5bd7ddef37efdf4d4942b46
Nikita Mayorov <nsmayorov@gmail.com>
2023-09-10 19:03:16 +0300

    add registration components '

---
+++ src/components/registration/ForgotPassword1/ForgotPassword1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ForgotPassword1 = () => {
+
+    return (
+        <>
+            <h1>Восстановление пароля</h1>
+        </>
+    )
+}
---
+++ src/components/registration/ForgotPassword2/ForgotPassword2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ForgotPassword2 = () => {
+
+    return (
+        <>
+          <h1>Восстановление пароля</h1>
+        </>
+    )
+}
